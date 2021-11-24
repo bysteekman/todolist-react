e9550d5a2e222bab8b5346c7377c860600f6ed85
Yan <bysteekman@gmail.com>
2021-11-24 10:44:28 +0200

    create fixed sidebar

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,21 +3,19 @@ import React from "react";
 const Aside = () => {
     return (
         <aside className="task_list">
-            <h1>Todo List</h1>
-            <ul>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-                <li>Task Name</li>
-            </ul>
+            <h1>Todo Lists</h1>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
+                <p>Task Name</p>
             <h3>Get All Task</h3>
         </aside>
     )
